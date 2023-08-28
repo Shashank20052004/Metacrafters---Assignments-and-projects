30d6eb2ec35c6da617d5b3398d2c2d9c7f7ed08b
Shashank20052004 <140271859+Shashank20052004@users.noreply.github.com>
2023-08-28 18:25:04 +0530

    FunctionErrorHandling.sol

--- ETH + AVAX - Proof/Module 1/FunctionErrorHandling.sol
+++ ETH + AVAX - Proof/Module 1/FunctionErrorHandling.sol
@@ -8,19 +8,12 @@ contract AgeVerification {
         minAge = _minAge; // Initialize the contract with a minimum age requirement
     }
 
-    // Modifier to verify whether the user's age meets the minimum requirement
-    modifier verifyAgeModifier(uint256 _userAge) {
-        require(_userAge >= minAge, "You must be at least the minimum age.");
-        _; // Continue executing the function if the age requirement is met
-    }
-
     // Function to check if a user's age grants access to content
     function checkAccess(uint256 _userAge) external view returns (string memory) {
         require(_userAge > 0, "Age must be greater than 0."); // Ensure age is valid
         
         assert(_userAge <= 100); // Assert that age is within a reasonable range (for example purposes)
-        assert(_userAge >= minAge); // Additional assert to ensure age meets the requirement
-
+        
         if (_userAge >= minAge) {
             return "Access granted. You can now view the content."; // Age meets the requirement
         } else {
